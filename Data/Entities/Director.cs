72e44d1d0458a1da05d96eba51d377385e024f7f
DanyaCt <sebtec1212@gmail.com>
2021-12-12 17:53:53 +0200

    added Director, Actor, ScenarioWriter

---
+++ Data/Entities/Director.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+	public class Director
+	{
+		public int Id { get; set; }
+
+		[Column(TypeName = "nvarchar(50)")]
+		public string FirstName { get; set; }
+
+		[Column(TypeName = "nvarchar(50)")]
+		public string LastName { get; set; }
+
+		public ICollection<Movie> Movies { get; set; }
+	}
+}
