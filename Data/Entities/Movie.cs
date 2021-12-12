448e4fc778531a200c2dcb0c9ef81ad3bf8edbe8
Chernikov Nikita <14nik20@gmail.com>
2021-12-12 17:36:17 +0200

    Updated Movie

--- Data/Entities/Movie.cs
+++ Data/Entities/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace Data.Entities
     {
         public int Id { get; set; }
 
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
 
+        public short AgeRestriction { get; set; }
+
+        public string Description { get; set; }
+
+        public short Length { get; set; }
+
+        public short Rating { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string LinkToPoster { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string LinkToAffiche { get; set; }
+
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
     }
 }
