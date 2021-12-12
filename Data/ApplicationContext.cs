21e8ba0259b74a4c911aa1b070e6706c4832cea2
Chernikov Nikita <14nik20@gmail.com>
2021-12-12 20:27:33 +0200

    ApplicationContext done

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,47 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Director> Directors { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Row> Rows { get; set; }
+
+        public DbSet<ScenarioWriter> ScenarioWriters { get; set; }
+
+        public DbSet<Seat> Seats { get; set; }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        public DbSet<SessionSeat> SessionSeats { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<SessionSeat>()
+            .HasKey(s => new { s.SessionId, s.SeatId });
+            builder.Entity<Review>()
+            .HasIndex(s => new { s.MovieId, s.UserId }).IsUnique();
+            builder.Entity<Country>()
+            .HasIndex(s => s.Name).IsUnique();
+        }
+    }
+}
--- Data/Entities/Row.cs
+++ Data/Entities/Row.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Data.Entities
     {
         public short Id { get; set; }
 
+        [Column(TypeName = "money")]
         public decimal Price { get; set; }
 
         public ICollection<Seat> Seats { get; set; }
