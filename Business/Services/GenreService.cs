16ed188638ff4812f66379769c358318b9858169
Chernikov Nikita <14nik20@gmail.com>
2021-12-12 21:48:16 +0200

    GenreService done

--- Business/Services/GenreService.cs
+++ Business/Services/GenreService.cs
@@ -5,6 +5,7 @@ using Data;
 using Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,32 +36,83 @@ namespace Business.Services
 
         public async Task AddMovieToGenreAsync(GenreModel genre, int movieId)
         {
-            throw new NotImplementedException();
+            if (genre is null)
+                throw new ArgumentNullException(nameof(genre), "Model cannot be null");
+            if (string.IsNullOrEmpty(genre.Name))
+                throw new ArgumentNullException(nameof(genre), "Model's name cannot be null or empty");
+            if (genre.MovieIds is null)
+                throw new ArgumentNullException(nameof(genre), "Model's Movie Ids cannot be null or empty");
+            var existingModel = await _context.Genres
+                .Include(g => g.Movies)
+                .Include(g => g.Users)
+                .SingleOrDefaultAsync(v => v.Id == genre.Id);
+            if (existingModel is null)
+                throw new ArgumentException("Model was not found", nameof(genre));
+            var movie = await _context.Movies.FindAsync(movieId);
+            if (movie is null)
+                throw new ArgumentException("Movie with such an Id was not found", nameof(movieId));
+            existingModel.Movies.Add(movie);
+            await _context.SaveChangesAsync();
         }
 
         public async Task AddUserToGenreAsync(GenreModel genre, string userId)
         {
-            throw new NotImplementedException();
+            if (genre is null)
+                throw new ArgumentNullException(nameof(genre), "Model cannot be null");
+            if (string.IsNullOrEmpty(genre.Name))
+                throw new ArgumentNullException(nameof(genre), "Model's name cannot be null or empty");
+            if (genre.MovieIds is null)
+                throw new ArgumentNullException(nameof(genre), "Model's Movie Ids cannot be null or empty");
+            var existingModel = await _context.Genres
+                .Include(g => g.Movies)
+                .Include(g => g.Users)
+                .SingleOrDefaultAsync(v => v.Id == genre.Id);
+            if (existingModel is null)
+                throw new ArgumentException("Model was not found", nameof(genre));
+            var user = await _context.Users.FindAsync(userId);
+            if (user is null)
+                throw new ArgumentException("User with such an Id was not found", nameof(userId));
+            existingModel.Users.Add(user);
+            await _context.SaveChangesAsync();
         }
 
         public async Task DeleteByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var model = await _context.Genres.FindAsync(id);
+            if (model is null)
+                throw new InvalidOperationException("Model with such an id was not found");
+            _context.Genres.Remove(model);
+            await _context.SaveChangesAsync();
         }
 
         public IEnumerable<GenreModel> GetAll()
         {
-            throw new NotImplementedException();
+            return _mapper.Map<IEnumerable<GenreModel>>(_context.Genres.Include(g => g.Movies).Include(g => g.Users));
         }
 
         public async Task<GenreModel> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return _mapper.Map<GenreModel>(await _context.Genres
+                .Include(g => g.Movies)
+                .Include(g => g.Users)
+                .SingleOrDefaultAsync(v => v.Id == id));
         }
 
         public async Task UpdateAsync(GenreModel model)
         {
-            throw new NotImplementedException();
+            if (model is null)
+                throw new ArgumentNullException(nameof(model), "Model cannot be null");
+            if (string.IsNullOrEmpty(model.Name))
+                throw new ArgumentNullException(nameof(model), "Model's name cannot be null or empty");
+            var existingModel = await _context.Genres
+                .Include(g => g.Movies)
+                .Include(g => g.Users)
+                .SingleOrDefaultAsync(v => v.Id == model.Id);
+            if (existingModel is null)
+                throw new ArgumentException("Model was not found", nameof(model));
+            existingModel = _mapper.Map(model, existingModel);
+            _context.Genres.Update(existingModel);
+            await _context.SaveChangesAsync();
         }
     }
 }
