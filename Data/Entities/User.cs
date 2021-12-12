ed3e346dd3e2db628ce7a5faf2d77fa91dee3dcd
Chernikov Nikita <14nik20@gmail.com>
2021-12-12 17:31:55 +0200

    Added Movie

---
+++ Data/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+    }
+}
