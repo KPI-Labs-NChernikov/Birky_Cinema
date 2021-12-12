b45308e3ad3a4d6c76fd712bbd4eefc6006eaa84
Chernikov Nikita <14nik20@gmail.com>
2021-12-12 21:29:09 +0200

    Fixed bugs

--- Business/AutoMapperProfile.cs
+++ Business/AutoMapperProfile.cs
@@ -1,29 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AutoMapper;
+using Business.Models;
+using Data.Entities;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Business
 {
-    using AutoMapper;
-    using DAL.Entities;
-    using BLL.Models;
-    using System.Linq;
-    using Data.Entities;
-    using Business.Models;
-
-    namespace BLL
+    public class AutoMapperProfile : Profile
     {
-        public class AutoMapperProfile : Profile
+        public AutoMapperProfile()
         {
-            public AutoMapperProfile()
-            {
-                CreateMap<Genre, GenreModel>()
-                    .ForMember(p => p.MovieIds, c => c.MapFrom(v => v.Movies.Select(m => m.Id)))
-                    .ForMember(p => p.UserIds, c => c.MapFrom(v => v.Users.Select(m => m.Id)))
-                    .ReverseMap();
-            }
+            CreateMap<Genre, GenreModel>()
+                .ForMember(p => p.MovieIds, c => c.MapFrom(v => v.Movies.Select(m => m.Id)))
+                .ForMember(p => p.UserIds, c => c.MapFrom(v => v.Users.Select(m => m.Id)))
+                .ReverseMap();
         }
     }
 }
