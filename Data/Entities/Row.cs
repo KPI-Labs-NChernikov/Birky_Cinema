daf24554aeaf050cc47478d3bd88da8093cfb6b5
Chernikov Nikita <14nik20@gmail.com>
2021-12-12 17:16:44 +0200

    Started doing DAL

---
+++ Data/Entities/Row.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Row
+    {
+        public short Id { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Seat> Seats { get; set; }
+    }
+}
---
+++ Data/Entities/Seat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+
+        public short PlaceNumber { get; set; }
+
+
+        public short RowId { get; set; }
+        public Row Row { get; set; }
+    }
+}
