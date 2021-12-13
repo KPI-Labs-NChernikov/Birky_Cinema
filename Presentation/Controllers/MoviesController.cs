50bbf9258475fc34ff4864cf51ff482d0e80cec6
Chernikov Nikita <14nik20@gmail.com>
2021-12-13 23:06:28 +0200

    Added working film carousels

---
+++ Presentation/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [Route("{controller}/{id}")]
+        public async Task<IActionResult> Index(int id, string lang)
+        {
+            ViewBag.Lang = (lang is null) ? "ukr" : lang;
+            return View();
+        }
+    }
+}
